cdf1f32118c7e4ede02141e04b3f3fff23a25550
berngarciamadrid <bernabemadrid@gmail.com>
2018-07-18 17:26:40 +0200

    Variable de Entorno MONGO Produccion

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/supercafe'
 } else {
-    urlDB = 'mongodb://cafe-user:A123456789@ds121575.mlab.com:21575/supercafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB;
